404e18a9c9dacf6c0b04fcf20daa87721cf9b67c
srjbora <suraj.singh@elucidata.io>
2019-07-24 00:09:02 +0530

    Added solution for removing duplicates

---
+++ 83.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* curr= head;
+        
+        while( curr && curr->next ) {
+            if( curr->val == curr->next->val ) {
+                ListNode* toBeDeleted = curr->next;
+                curr->next = curr->next->next;
+                delete toBeDeleted;
+                toBeDeleted = nullptr;
+            } else {
+                curr= curr->next;
+            }
+        }
+        
+        return head;
+    }
+};
