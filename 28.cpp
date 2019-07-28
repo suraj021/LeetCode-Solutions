81b24f49a19c786809afc1497a87f2f158c798fc
suraj021 <surajbora021@gmail.com>
2019-07-28 11:51:39 +0530

    Changed substring problem solution from brute force to KMP

--- 28.cpp
+++ 28.cpp
@@ -1,17 +1,53 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int m= (int)haystack.length();
-        int n= (int)needle.length();
+        int m= haystack.length();
+        int n= needle.length();
         
-        for( int i= 0; i<= m-n; ++i ){
-            int j= 0;
-            while( j < n && haystack[i+j]== needle[j] )
-                j++;
-            if( j== n )
-                return i;
+        if( n== 0 )
+            return 0;
+        
+        vector<int> lps = calculateLPS(needle);
+        
+        int i= 0, j=0;
+        while( i < m ) {
+            if( haystack[i] == needle[j] ) {
+                i++; j++;
+                
+                if( j== n ) {
+                    return ( i-j );
+                }
+            } else {
+                if( j!= 0 ) {
+                    j= lps[j-1];
+                } else {
+                    i+= 1;
+                }
+            }
         }
-        
         return -1;
+    }
+    
+    vector<int> calculateLPS(string s) {
+        int n= s.length();
+        vector<int> lps(n);
+        
+        lps[0]= 0;
+        int i= 0, j= 1;
+        
+        while( j < n ) {
+            if( s[j] == s[i] ) {
+                lps[j]= i+1;
+                j++; i++;
+            } else {
+                if( i!= 0 ) {
+                    i= lps[i-1];
+                } else {
+                    lps[j] = 0;
+                    j++;
+                }
+            }
+        }
+        return lps;
     }
 };
