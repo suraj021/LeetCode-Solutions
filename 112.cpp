b92e8b7d51d8e4e0990faebe0ed92f9b1e7fd110
suraj021 <surajbora021@gmail.com>
2019-08-08 22:51:51 +0530

    Added iterative method for path sum

--- 112.cpp
+++ 112.cpp
@@ -10,12 +10,42 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int sum) {
+        
+        if( root== NULL ) 
+            return false;
+        
+        stack<TreeNode*> node_stack;
+        stack<int> sum_stack;
+        
+        node_stack.push(root); sum_stack.push(root->val);
+        
+        while( !node_stack.empty() ) {
+            TreeNode* curr= node_stack.top(); node_stack.pop();
+            int path_sum= sum_stack.top(); sum_stack.pop();
+            
+            if(!curr->left && !curr->right && path_sum == sum )
+                return true;
+            
+            if(curr->left) {
+                node_stack.push(curr->left);
+                sum_stack.push(path_sum + curr->left->val);
+            }
+            if(curr->right){
+                node_stack.push(curr->right);
+                sum_stack.push(path_sum + curr->right->val);
+            }
+        }
+        
+        return false;
+    }
+    
+    bool _hasPathSum(TreeNode* root, int sum) {
         if( root== NULL )
             return false;
         if( !root->left && !root->right )
             return root->val == sum;
 
-        return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
+        return _hasPathSum(root->left, sum-root->val) || _hasPathSum(root->right, sum-root->val);
     }
     
 };
