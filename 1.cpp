8216045b5c6f8a6184c009aed73f35c1e766fec8
suraj021 <surajbora021@gmail.com>
2019-08-26 20:10:03 +0530

    Usinng map to findtwo numbers

--- 1.cpp
+++ 1.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        vector<int> ans;
         
-        int n= (int)nums.size();
-        int x, y;
-        
-        for( int i= 0; i< n; ++i ){
-            for( int j= i+1; j < n; ++j ){
-                if( nums[i]+nums[j]== target ){
-                    x= i;
-                    y= j;
-                    break;
-                }
+        for( int i= 0; i< nums.size(); ++i){
+            if(mp.find(target - nums[i]) != mp.end()){
+                ans.push_back(mp[target - nums[i]]);
+                ans.push_back(i);
+                break;
             }
+            mp[nums[i]]= i;
         }
         
-        vector<int> ans( {x, y} );
-        
         return ans;
-        
     }
 };
