7fb1423051c2bc8b5d678042c840559cc1081151
suraj021 <surajbora021@gmail.com>
2019-11-27 23:57:35 +0530

    remove nth node from end of linked list

--- 19.cpp
+++ 19.cpp
@@ -9,47 +9,27 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int len= length( head );
-        int k= len-n;
+        ListNode* dummy= new ListNode(0);
+        dummy->next = head;
+        ListNode *slow, *fast;
+        slow= fast= dummy;
         
-        if( len== 0 )
-            return NULL;
-        
-        if( k== 0 ){
-            ListNode* curr= head->next;
-            head->next= NULL;
-            delete head;
-            return curr;
-        }else if( n== 1 ){
-            ListNode* curr= head;
-            for( int i= 1; i< len-1; ++i )
-                curr= curr->next;
-            ListNode* del= curr->next;
-            curr->next= NULL;
-            
-            delete del;
-        }else{
-            ListNode* curr= head;
-            for( int i= 1; i< k; ++i )
-                curr= curr->next;
-                
-            ListNode* del= curr->next;
-            curr->next= del->next;
-            
-            delete del;
-            
+        for(int i= 0; i< n && fast->next; ++i){
+            fast= fast->next;
         }
         
-        return head;
-    }
-    
-    int length( ListNode* head ){
-        int l= 0;
-        
-        while( head!= NULL ){
-            l++;
-            head= head->next;
+        while(fast && fast->next){
+            slow= slow->next;
+            fast= fast->next;
         }
-        return l;
+        
+        ListNode* ptr= slow->next;
+        
+        slow->next= slow->next->next;
+        
+        delete ptr;
+        ptr= nullptr;
+        
+        return dummy->next;
     }
 };
