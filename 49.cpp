01ab941bec873eaf51a8416edf0af802570d6106
suraj021 <surajbora021@gmail.com>
2019-12-22 12:17:43 +0530

    string sorting i=using counting sort

--- 49.cpp
+++ 49.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& str) {
-        int n= (int)str.size();
-        vector< vector<string> > ans;
-        unordered_map< string, vector<string> > mp;
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> ump;
+        vector<vector<string>> ans;
         
-        if( n== 0 )
-            return ans;
-        
-        for( int i= 0; i< n; ++i ){
-            string s= str[i];
-            sort( s.begin(), s.end() );
-            mp[s].push_back( str[i] );
+        for(string word: strs){
+            string s= word;
+            s= counting_sort(s);
+            ump[s].push_back(word);
         }
         
-        for( auto &it: mp ){
-            vector< string > vs= it.second;
-            ans.push_back( vs );
+        for(auto it= ump.begin(); it!= ump.end(); ++it){
+            ans.push_back(it->second);   
         }
         
         return ans;
+    }
+    
+    // counting sort to increase effeciency
+    string counting_sort(string s){
+        int f[26]= {0};
+        
+        for(char c: s){
+            f[c-'a']++;
+        }
+        
+        s= "";
+        
+        for(int i= 0; i< 26; ++i){
+            for(int j= 0; j< f[i]; ++j){
+                s+= char(i + 'a');
+            }
+        }
+        
+        return s;
     }
 };
