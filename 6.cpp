5adfd6d84e9f4b54a21b3ed9cac2f251dc4ac33b
suraj021 <surajbora021@gmail.com>
2019-12-26 22:08:41 +0530

    optimized zig zag conversion

--- 6.cpp
+++ 6.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        vector<char> sol[numRows+1];
-        int n= (int)s.length();
+        vector<string> v(numRows+1);
         int row= 1;
         int cycle= 0;
         
-        if( numRows== 1 )
-            return s;
+        if(numRows == 1) return s;
         
-        for( int i= 0; i< n; ++i ){
-           
-            sol[row].push_back( s[i] );
+        for(int i= 0; i< s.length(); ++i){
+            v[row].push_back(s[i]);
             
-            if( row== numRows )
+            if(row== numRows){
                 cycle= 1;
-            if( row== 1 )
+            }
+            if(row == 1){
                 cycle= 0;
-            
-            if( cycle ){
-                row--;
-            }else{
-                row++;
             }
             
+            if(cycle) row--;
+            else row++;
         }
         
-        string ans= "";
+        s= "";
         
-        for( int i= 1; i<= numRows; ++i ){
-            for( int j= 0; j< sol[i].size(); ++j ){
-                ans+= sol[i][j];
-            }
+        for(string c: v){
+            s+= c;
         }
         
-        return ans;
+        return s;
     }
 };
