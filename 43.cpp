5776e45011eebe1de4600e0bb762fe8911b7bfb1
suraj021 <surajbora021@gmail.com>
2019-12-28 15:10:17 +0530

    trick to multiply bignumbers

--- 43.cpp
+++ 43.cpp
@@ -49,5 +49,24 @@ public:
                 
         return string(n, '0') + ans;
     }
+
+    // O(n^2) method trick
+    string _multiply(string num1, string num2) {
+        int m = num1.size(), n = num2.size();
+        string ans(m + n, '0');
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int sum = (num1[i] - '0') * (num2[j] - '0') + (ans[i + j + 1] - '0');
+                ans[i + j + 1] = sum % 10 + '0';
+                ans[i + j] += sum / 10;
+            }
+        }
+        for (int i = 0; i < m + n; i++) {
+            if (ans[i] != '0') {
+                return ans.substr(i);
+            }
+        }
+        return "0";
+    }
     
 };
