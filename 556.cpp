e0d8167b36d5d98003ff578262ec8d25b2f06cf2
suraj021 <surajbora021@gmail.com>
2019-12-28 18:39:00 +0530

    implemented next permutation

--- 556.cpp
+++ 556.cpp
@@ -9,4 +9,38 @@ public:
         
         return num > INT_MAX || num <= n ? -1 : num;
     }
+
+    int _nextGreaterElement(long n) {
+        string s= to_string(n);
+        
+        if(!nextPermutation(s)) return -1;
+        
+        auto num= stoll(s);
+        
+        return num > INT_MAX || num <= n ? -1 : num;
+    }
+    
+    bool nextPermutation(string &s){
+        int n= (int)s.length();
+        int i; // max index for which s[i] > s[i-1]
+        
+        for(i= n-1; i> 0; --i){
+            if(s[i] > s[i-1]) break;
+        }
+                
+        if(i== 0)   return false;
+        
+        int num= s[i-1];
+        int smallest= i;
+        
+        for(int j= i+1; j< n; ++j){
+            if(num < s[j] && s[j] < s[smallest])
+                smallest= j;
+        }
+            
+        swap(s[i-1], s[smallest]);        
+        sort(s.begin()+i, s.end());
+        
+        return true;
+    }
 };
