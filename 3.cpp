7bf3c1e6dd7954e31a2f6093ed137d6f709986f0
suraj021 <surajbora021@gmail.com>
2019-12-28 20:50:38 +0530

    Longest Substring Without Repeating Characters using sliding window

--- 3.cpp
+++ 3.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        if(s.length()== 0) return 0;
+        vector<int> lastSeen(256, -1); // last seen of a char
+        int curr= 0; // start of the current unique substring
+        int ans= 1; // initial ans
+        lastSeen[s[0]]= 0; 
         
-        int n= (int)s.length();
-
-        if( n== 1 )
-            return 1;
-            
-        int ans= 0;
-
-        int f= 1, b= 0;
-        set<char> ss;
-
-        ss.insert(s[0]);
-
-        while( f < n ){
-            pair< set<char>::iterator, bool > p= ss.insert( s[f] );
-            if( p.second!= false ){
-                int l= f-b+1;
-                if( l > ans )
-                    ans= l;
-                f++;
-            }else{
-                ss.erase( s[b] );
-                b++;
+        // we look for the  window [curr, i]
+        
+        for(int i= 1; i< s.length(); ++i){
+            if(lastSeen[s[i]] >= 0){  // if we have already seen the char
+                curr= max(lastSeen[s[i]] + 1, curr);
             }
-
+            lastSeen[s[i]] = i; // update the last seen
+            ans= max(ans, i-curr+1);
         }
-
+        
         return ans;
     }
 };
