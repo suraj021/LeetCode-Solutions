8c6d0cc6e2315bcc8ec5e51e0a3c34ae9c7980e3
suraj021 <surajbora021@gmail.com>
2020-01-04 23:25:19 +0530

    Flipping an Image

---
+++ 832.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+        for (auto& row : A) reverse(row.begin(), row.end());
+        for (auto& row : A) for (int& i: row) i ^= 1;
+        
+        return A;        
+    }
+};
